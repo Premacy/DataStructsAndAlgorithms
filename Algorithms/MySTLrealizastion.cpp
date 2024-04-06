78d727df04a585406bbdb752a3936f5fd4a28884
Premacy <116500284+Premacy@users.noreply.github.com>
2024-04-06 19:05:29 +0700

    Update MySTLrealizastion.cpp

--- Algorithms/MySTLrealizastion.cpp
+++ Algorithms/MySTLrealizastion.cpp
@@ -1,1 +1,38 @@
 
+template<class InputIterator>
+void double_each_element(InputIterator begin, InputIterator end){
+    for(auto itr = begin; itr != end; ++itr){
+        (*itr) *= 2;
+    }
+
+    /*
+    for(auto& val : container){
+        val *= 2
+    }
+    */
+}
+
+template<class Containter>
+void printContainer(const Containter &container, std::ostream &stream){
+    for(const auto& val : container){
+        stream << val << ", ";
+    }
+    stream << "\n";
+}
+
+template<class Itr, class UnaryPredicate>
+Itr find_if(Itr first, Itr last, UnaryPredicate p){
+  auto Itr itr = first;
+  while(itr != last){
+    if(p(*itr)) return itr;
+    ++itr;
+  }
+  return itr;
+}
+
+template<class It, class U>
+It find_if_not(It first, It last, U p) {
+  auto result = std::find_if(first, last, [&](auto&& e){ return !p(e); }); 
+
+  return result;
+}
